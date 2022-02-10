bafdf140cc6f7b3a9f929886f489f7d5791c3816
21IST2Flerov <99124529+21IST2Flerov@users.noreply.github.com>
2022-02-10 23:38:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,11 @@ int gcd(int a, int b)
 
 
 {
-    while (a != 0 && b != 0) {
-  if ( a > b )
-  a %= b;
-  else
-  b %= a;
-  }
-  return a + b;
+     if (a % b == 0)
+    return b;
+  if (b % a == 0)
+    return a;
+  if (a > b)
+    return gcd(a%b, b);
+  return gcd(a, b%a);
 }
